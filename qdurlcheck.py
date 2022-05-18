d2ef556f65a2344ff239251cfab34fe15142e208
Antonino Esposito <antonino.esposito@infocube.it>
2022-05-18 17:25:56 +0200

    Cachet support script to monitor URLs

---
+++ qdurlcheck.py
@@ -0,0 +1,38 @@
+import requests
+import sys 
+
+x = requests.head(sys.argv[1])
+print(x.status_code)
+
+
+if x.status_code == 200:
+   ##
+   ##  Update component status
+   ##
+   url = "http://YYYYYYYYYYYYYYYYYYYYYYYYYYYYY/api/v1/components/" + sys.argv[2]
+   headers = {"Accept": "application/json", "Content-Type": "application/json", "X-Cachet-Application": "Qadisha", "X-Cachet-Token": "XXXXXXXXXXXXXXXXXXX"}
+   dataj = {
+       "order": 0,
+       "group_id": sys.argv[3],
+       "enabled": True,
+       "status": 1
+   }
+   response = requests.put(url, headers=headers, json=dataj)
+   print(response.text)
+   print(dataj)
+else:
+   ##
+   ##  Update component status
+   ##
+   url = "http://YYYYYYYYYYYYYYYYYYYYYYYYYYYYY/api/v1/components/" + sys.argv[2]
+   headers = {"Accept": "application/json", "Content-Type": "application/json", "X-Cachet-Application": "Qadisha", "X-Cachet-Token": "XXXXXXXXXXXXXXXXXXX"}
+   dataj = {
+       "order": 0,
+       "group_id": sys.argv[3],
+       "enabled": True,
+       "status": 4
+   }
+   response = requests.put(url, headers=headers, json=dataj)
+   print(response.text)
+   print(dataj)
+
