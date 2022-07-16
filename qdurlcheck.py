e67699b0759eb1841aeba38b6e8ca2d8c19c0beb
Antonino Esposito <antonino.esposito@infocube.it>
2022-07-16 21:11:29 +0200

    Add incident creation

--- qdurlcheck.py
+++ qdurlcheck.py
@@ -1,5 +1,6 @@
 import requests
-import sys 
+import sys
+from datetime import date
 
 h = {
     "Cache-Control": "no-cache",
@@ -40,3 +41,17 @@ else:
    print(response.text)
    print(dataj)
 
+   ##
+   ##  Open incident
+   ##
+   url = "http://YYYYYYYYYYYYYYYYYYYYYYYYYYYYY/api/v1/incidents"
+   payload = {
+       "visible": 1,
+       "notify": True,
+       "name": sys.argv[1],
+       "message": "Our team is working on the issue, we'll come back with an update as soon as possible.",
+       "status": 1
+   }
+   headers = {"Accept": "application/json", "Content-Type": "application/json", "X-Cachet-Application": "Qadisha", "X-Cachet-Token": "XXXXXXXXXXXXXXXXXXX"}
+   response = requests.post(url, json=payload, headers=headers)
+   print(response.text)
