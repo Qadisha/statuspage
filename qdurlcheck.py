4a84fc16bfc349d07b9b45b8e89515090f5e8fde
Antonino Esposito <antonino.esposito@infocube.it>
2022-09-03 07:44:30 +0200

    Fix the request HTTP error

--- qdurlcheck.py
+++ qdurlcheck.py
@@ -2,84 +2,63 @@ import requests
 import sys
 from datetime import date
 
-def make_request():
-    try:
-        h = {
-            "Cache-Control": "no-cache",
-            "Pragma": "no-cache"
-        }
-        x = requests.head(sys.argv[1], headers=h)
-        print(x.status_code)
+h = {
+    "Cache-Control": "no-cache",
+    "Pragma": "no-cache"
+}
 
-        if x.status_code == 200:
-        ##
-        ##  Update component status
-        ##
-        url = "http://YYYYYYYYYYYYYYYYYYYYYYYYYYYYY/api/v1/components/" + sys.argv[2]
-        headers = {"Accept": "application/json", "Content-Type": "application/json", "X-Cachet-Application": "Qadisha", "X-Cachet-Token": "XXXXXXXXXXXXXXXXXXX"}
-        dataj = {
-            "order": 0,
-            "group_id": sys.argv[3],
-            "enabled": True,
-            "status": 1
-        }
-        response = requests.put(url, headers=headers, json=dataj)
-        print(response.text)
-        print(dataj)
-        else:
-        ##
-        ##  Update component status
-        ##
-        url = "http://YYYYYYYYYYYYYYYYYYYYYYYYYYYYY/api/v1/components/" + sys.argv[2]
-        headers = {"Accept": "application/json", "Content-Type": "application/json", "X-Cachet-Application": "Qadisha", "X-Cachet-Token": "XXXXXXXXXXXXXXXXXXX"}
-        dataj = {
-            "order": 0,
-            "group_id": sys.argv[3],
-            "enabled": True,
-            "status": 4
-        }
-        response = requests.put(url, headers=headers, json=dataj)
-        print(response.text)
-        print(dataj)
-
-        ##
-        ##  Open incident
-        ##
-        url = "http://YYYYYYYYYYYYYYYYYYYYYYYYYYYYY/api/v1/incidents"
-        payload = {
-            "visible": 1,
-            "notify": True,
-            "name": sys.argv[1],
-            "message": "Our team is working on the issue, we'll come back with an update as soon as possible.",
-            "status": 1
-        }
-        headers = {"Accept": "application/json", "Content-Type": "application/json", "X-Cachet-Application": "Qadisha", "X-Cachet-Token": "XXXXXXXXXXXXXXXXXXX"}
-        response = requests.post(url, json=payload, headers=headers)
-        print(response.text)
+try:
+    x = requests.head(sys.argv[1], headers=h, stream=True, timeout=10)
+    print(x.status_code)
+except requests.exceptions.HTTPError as err:
+    print("HTTP Error")
+except requests.exceptions.RequestException as e:
+    print("Other Exception")
 
 
 
-    except requests.exceptions.ConnectionError:        
-        print('connection error occurred')
+    if x.status_code == 200:
+    ##
+    ##  Update component status
+    ##
+    url = "http://YYYYYYYYYYYYYYYYYYYYYYYYYYYYY/api/v1/components/" + sys.argv[2]
+    headers = {"Accept": "application/json", "Content-Type": "application/json", "X-Cachet-Application": "Qadisha", "X-Cachet-Token": "XXXXXXXXXXXXXXXXXXX"}
+    dataj = {
+        "order": 0,
+        "group_id": sys.argv[3],
+        "enabled": True,
+        "status": 1
+    }
+    response = requests.put(url, headers=headers, json=dataj)
+    print(response.text)
+    print(dataj)
+    else:
+    ##
+    ##  Update component status
+    ##
+    url = "http://YYYYYYYYYYYYYYYYYYYYYYYYYYYYY/api/v1/components/" + sys.argv[2]
+    headers = {"Accept": "application/json", "Content-Type": "application/json", "X-Cachet-Application": "Qadisha", "X-Cachet-Token": "XXXXXXXXXXXXXXXXXXX"}
+    dataj = {
+        "order": 0,
+        "group_id": sys.argv[3],
+        "enabled": True,
+        "status": 4
+    }
+    response = requests.put(url, headers=headers, json=dataj)
+    print(response.text)
+    print(dataj)
 
-        ##
-        ##  Open incident
-        ##
-        url = "http://YYYYYYYYYYYYYYYYYYYYYYYYYYYYY/api/v1/incidents"
-        payload = {
-            "visible": 1,
-            "notify": True,
-            "name": sys.argv[1],
-            "message": "Our team is working on the issue, we'll come back with an update as soon as possible.",
-            "status": 1
-        }
-        headers = {"Accept": "application/json", "Content-Type": "application/json", "X-Cachet-Application": "Qadisha", "X-Cachet-Token": "XXXXXXXXXXXXXXXXXXX"}
-        response = requests.post(url, json=payload, headers=headers)
-        print(response.text)
-
-make_request()
-
-
-
-
-
+    ##
+    ##  Open incident
+    ##
+    url = "http://YYYYYYYYYYYYYYYYYYYYYYYYYYYYY/api/v1/incidents"
+    payload = {
+        "visible": 1,
+        "notify": True,
+        "name": sys.argv[1],
+        "message": "Our team is working on the issue, we'll come back with an update as soon as possible.",
+        "status": 1
+    }
+    headers = {"Accept": "application/json", "Content-Type": "application/json", "X-Cachet-Application": "Qadisha", "X-Cachet-Token": "XXXXXXXXXXXXXXXXXXX"}
+    response = requests.post(url, json=payload, headers=headers)
+    print(response.text)
